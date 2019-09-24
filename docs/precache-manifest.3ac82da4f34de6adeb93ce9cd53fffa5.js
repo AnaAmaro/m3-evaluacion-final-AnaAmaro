564c9dc77c58c1129bfd8714ce97ab02063f4fa0
Ana Amaro <amarovazquezana@gmail.com>
2019-09-24 14:29:44 +0200

    Build docs

---
+++ docs/precache-manifest.3ac82da4f34de6adeb93ce9cd53fffa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d7133f21961280854d8b70427656e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53ab331e5bdf2de26fb2",
+    "url": "./static/css/main.3102e17b.chunk.css"
+  },
+  {
+    "revision": "4d5e37dca99da823d62c",
+    "url": "./static/js/2.eca5bfd8.chunk.js"
+  },
+  {
+    "revision": "53ab331e5bdf2de26fb2",
+    "url": "./static/js/main.36722950.chunk.js"
+  },
+  {
+    "revision": "edc1b0f6e27e79526deb",
+    "url": "./static/js/runtime-main.6ef6ab0c.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Rickandmortylogo.75509f2e.png"
+  }
+]);
